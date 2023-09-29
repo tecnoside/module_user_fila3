9b35b5b97d72c9232be63dbc3ad067d70896cbf1
marco76tv <marco.sottana@gmail.com>
2023-09-29 15:17:50 +0200

    up

---
+++ Http/Controllers/Api/GetLoggedUserController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Http\Controllers\Api;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Modules\Xot\Datas\JsonResponseData;
+use Modules\Xot\Http\Controllers\XotBaseController;
+
+class GetLoggedUserController extends XotBaseController
+{
+    /**
+     * Login api.
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $user = $request->user();
+
+        return JsonResponseData::from([
+            'message' => 'logged user',
+            'data' => $user->toArray(),
+        ]);
+    }
+}

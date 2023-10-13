d1b2fc8b0643b53ce62a8b532e21cd5107954184
dadit97 <dadit97@users.noreply.github.com>
2023-10-13 16:50:53 +0000

    Check & fix styling

--- Exceptions/ProviderNotConfigured.php
+++ Exceptions/ProviderNotConfigured.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\User\Exceptions;
 
-use LogicException;
-
-class ProviderNotConfigured extends LogicException
+class ProviderNotConfigured extends \LogicException
 {
     public static function make(string $provider): static
     {
-        return new static('Provider "' . $provider . '" is not configured.');
+        return new static('Provider "'.$provider.'" is not configured.');
     }
 }

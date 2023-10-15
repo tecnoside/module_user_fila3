3b7395e8c80306b461782fa56becb4d6edb35853
marco76tv <marco76tv@users.noreply.github.com>
2023-10-15 10:15:39 +0000

    Dusting

--- Exceptions/ProviderNotConfigured.php
+++ Exceptions/ProviderNotConfigured.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\User\Exceptions;
 
-class ProviderNotConfigured extends \LogicException
+use LogicException;
+
+class ProviderNotConfigured extends LogicException
 {
     public static function make(string $provider): static
     {
-        return new static('Provider "'.$provider.'" is not configured.');
+        return new static('Provider "' . $provider . '" is not configured.');
     }
 }

936ec19d9409908c1293aa783092b84bf316c169
marco76tv <marco.sottana@gmail.com>
2023-11-02 11:50:43 +0100

    up

--- Http/Controllers/Api/LogoutController.php
+++ Http/Controllers/Api/LogoutController.php
@@ -26,8 +26,11 @@ class LogoutController extends XotBaseController
             ->where('access_token_id', $accessToken->id)
             ->delete();
         */
+        Assert::methodExists($accessToken, 'getKey');
+        Assert::methodExists($accessToken, 'delete');
         OauthRefreshToken::where('access_token_id', $accessToken->getKey())->delete();
-        $user->token()?->delete();
+        $accessToken->delete();
+        // $user->token()?->delete();
 
         MobileDeviceUser::where('user_id', $user->id)->update(['logout_at' => now()]);
 

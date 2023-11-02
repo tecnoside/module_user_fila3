73222548202d4a18c09eb57992b730140bad5dc1
marco76tv <marco76tv@users.noreply.github.com>
2023-11-02 15:03:01 +0000

    Check & fix styling

--- Http/Controllers/Socialite/LoginController.php
+++ Http/Controllers/Socialite/LoginController.php
@@ -56,8 +56,8 @@ class LoginController extends Controller
         $scopes = App(GetProviderScopesAction::class)->execute($provider);
         $socialite = Socialite::with($provider);
         // Assert::isInstanceOf($socialite, \Laravel\Socialite\Contracts\Provider::class);
-        //Assert::methodExists($socialite, 'scopes');
-        if(!method_exists($socialite, 'scopes')){
+        // Assert::methodExists($socialite, 'scopes');
+        if (! method_exists($socialite, 'scopes')) {
             throw new \Exception('wip');
         }
 

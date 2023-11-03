75e14c23388b7407e70230930c4b519875e21dbe
marco76tv <marco.sottana@gmail.com>
2023-11-03 11:15:27 +0100

    up

--- Http/Controllers/Socialite/LoginController.php
+++ Http/Controllers/Socialite/LoginController.php
@@ -56,6 +56,9 @@ class LoginController extends Controller
         /** @var Socialite $socialite */
         // Assert::isInstanceOf($socialite, \Laravel\Socialite\Contracts\Provider::class);
         // Assert::methodExists($socialite, 'scopes');
+        if(!is_object($socialite)){
+            throw new \Exception('WIP');
+        }
         if (! method_exists(get_class($socialite), 'scopes')) {
             throw new \Exception('wip');
         }

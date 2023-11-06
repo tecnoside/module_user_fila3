d842b12352b9f2e3b6463d6afb47bbdebb15c214
samueleorfei <samueleorfei@users.noreply.github.com>
2023-11-06 10:54:56 +0000

    Check & fix styling

--- Models/User.php
+++ Models/User.php
@@ -196,7 +196,7 @@ class User extends Authenticatable implements UserContract
 
     public function canAccessPanel(Panel $panel): bool
     {
-        if ($panel->getId() !== 'admin') {
+        if ('admin' !== $panel->getId()) {
             $role = $panel->getId();
             /*
             $xot = XotData::make();

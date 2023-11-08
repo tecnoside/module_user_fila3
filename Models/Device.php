6e5957bd4d75298e679e478151d789513f5168b6
marco76tv <marco.sottana@gmail.com>
2023-11-08 18:56:01 +0100

    up

--- Models/Device.php
+++ Models/Device.php
@@ -13,7 +13,7 @@ class Device extends BaseModel
      */
     protected $fillable = [
         'id',
-        'mobile_id',
+        'mobile_id', //mattia
         'languages', // ['en-us', 'en'] ;
         'device', // "Macintosh"
         'platform', // "OS X"
@@ -32,6 +32,7 @@ class Device extends BaseModel
      */
     protected $casts = [
         // 'id' => 'string',
-        'locales' => 'array',
+        //'locales' => 'array',
+        'languages'=> 'array',
     ];
 }

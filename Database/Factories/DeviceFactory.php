da7e9b8677bed12e08f6be5d7cc3573f087dc2bd
Malebestia <vair81@gmail.com>
2023-11-13 18:09:26 +0100

    up

--- Database/Factories/DeviceFactory.php
+++ Database/Factories/DeviceFactory.php
@@ -24,8 +24,8 @@ class DeviceFactory extends Factory
     public function definition()
     {
         return [
-            'id' => $this->faker->randomNumber,
-            'mobile_id' => $this->faker->integer,
+            // 'id' => $this->faker->randomNumber,
+            // 'mobile_id' => $this->faker->integer,
             'device' => $this->faker->word,
             'platform' => $this->faker->word,
             'browser' => $this->faker->word,

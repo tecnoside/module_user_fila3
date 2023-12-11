5ec54df24c3c36f6ed2722dcaad92d211c3f9ff2
marco76tv <marco.sottana@gmail.com>
2023-12-11 17:56:28 +0100

    add tenant

---
+++ Models/TenantUser.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Modules\User\Models\TenantUser.
+ *
+ * @method static Builder|TeamUser newModelQuery()
+ * @method static Builder|TeamUser newQuery()
+ * @method static Builder|TeamUser query()
+ *
+ * @property int                             $id
+ * @property string|null                     $tenant_id
+ * @property string|null                     $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null                     $created_by
+ * @property string|null                     $updated_by
+ *
+ * @method static Builder|TeamUser whereCreatedAt($value)
+ * @method static Builder|TeamUser whereCreatedBy($value)
+ * @method static Builder|TeamUser whereCustomerId($value)
+ * @method static Builder|TeamUser whereId($value)
+ * @method static Builder|TeamUser whereRole($value)
+ * @method static Builder|TeamUser whereTeamId($value)
+ * @method static Builder|TeamUser whereUpdatedAt($value)
+ * @method static Builder|TeamUser whereUpdatedBy($value)
+ * @method static Builder|TeamUser whereUserId($value)
+ * @method static Builder|TeamUser whereUuid($value)
+ *
+ * @mixin \Eloquent
+ */
+class TenantUser extends BasePivot
+{
+    protected $connection = 'user';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tenant_id',
+        'user_id',
+    ];
+}
--- Models/Traits/HasTenants.php
+++ Models/Traits/HasTenants.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Modules\User\Models\Traits;
 
 use Filament\Panel;
-use Illuminate\Database\Eloquent\Model;
+use Modules\Xot\Datas\XotData;
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Modules\User\Contracts\TeamContract;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 // use Modules\User\Models\OwnerRole;
 
@@ -32,5 +34,27 @@ trait HasTenants
     public function getTenants(Panel $panel): array|Collection
     {
         return $this->teams;
+    }
+
+    /**
+     * Get all of the teams the user belongs to.
+     */
+    public function tenants(): BelongsToMany
+    {
+        $xot = XotData::make();
+        $pivotClass = $xot->getTenantPivotClass();
+        $pivot = app($pivotClass);
+        $pivotTable = $pivot->getTable();
+        $pivotDbName = $pivot->getConnection()->getDatabaseName();
+        $pivotTableFull = $pivotDbName.'.'.$pivotTable;
+        $pivotFields = $pivot->getFillable();
+
+        // $this->setConnection('mysql');
+        return $this->belongsToMany($xot->getTenantClass(), $pivotTableFull, null, 'tenant_id')
+            ->using($pivotClass)
+            ->withPivot('role')
+            ->withTimestamps()
+            //->as('membership')
+            ;
     }
 }

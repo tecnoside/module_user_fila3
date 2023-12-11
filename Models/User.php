d65ca6fabdd9a8ef4c60653ff9fcf29e12dc8b29
samueleorfei <samueleorfei@users.noreply.github.com>
2023-12-11 14:35:18 +0000

    Check & fix styling

--- Models/User.php
+++ Models/User.php
@@ -207,7 +207,7 @@ class User extends Authenticatable implements HasName, UserContract, HasTenants
     public function canAccessPanel(Panel $panel): bool
     {
         // $panel->default('admin');
-        if ($panel->getId() !== 'admin') {
+        if ('admin' !== $panel->getId()) {
             $role = $panel->getId();
             /*
             $xot = XotData::make();
@@ -270,10 +270,10 @@ class User extends Authenticatable implements HasName, UserContract, HasTenants
 
     public function getFullNameAttribute(?string $value): ?string
     {
-        if ($value != null) {
+        if (null != $value) {
             return $value;
         }
 
-        return $this->first_name . ' ' . $this->last_name;
+        return $this->first_name.' '.$this->last_name;
     }
 }

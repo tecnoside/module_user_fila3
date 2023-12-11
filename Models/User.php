ea2cf745808cf72c96f302639bff22c3ea7c8151
marco76tv <marco76tv@users.noreply.github.com>
2023-12-11 16:12:19 +0000

    Check & fix styling

--- Models/User.php
+++ Models/User.php
@@ -206,7 +206,7 @@ class User extends Authenticatable implements HasName, HasTenants, UserContract
     public function canAccessPanel(Panel $panel): bool
     {
         // $panel->default('admin');
-        if ($panel->getId() !== 'admin') {
+        if ('admin' !== $panel->getId()) {
             $role = $panel->getId();
             /*
             $xot = XotData::make();
@@ -269,7 +269,7 @@ class User extends Authenticatable implements HasName, HasTenants, UserContract
 
     public function getFullNameAttribute(?string $value): ?string
     {
-        if ($value != null) {
+        if (null != $value) {
             return $value;
         }
 

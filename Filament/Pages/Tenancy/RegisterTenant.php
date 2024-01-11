a0867e5d172dfacaa5ca13062275b0ddbe6f2a64
marco76tv <marco76tv@users.noreply.github.com>
2024-01-11 10:51:10 +0000

    Dusting

--- Filament/Pages/Tenancy/RegisterTenant.php
+++ Filament/Pages/Tenancy/RegisterTenant.php
@@ -40,7 +40,7 @@ class RegisterTenant extends BaseRegisterTenant
     }
 
     /**
-     * @param array<string, mixed> $data
+     * @param  array<string, mixed>  $data
      */
     protected function handleRegistration(array $data): Model
     {

bc6ea46762475fd9999ffeaf5e0da1c83565e077
marco76tv <marco.sottana@gmail.com>
2024-02-13 18:16:44 +0100

    up

--- Console/Commands/CreateTenantCommand.php
+++ Console/Commands/CreateTenantCommand.php
@@ -29,7 +29,7 @@ class CreateTenantCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle():void
     {
         $modelClass = XotData::make()->getTenantClass();
 
--- Console/Commands/ShowTenantListCommand.php
+++ Console/Commands/ShowTenantListCommand.php
@@ -26,7 +26,7 @@ class ShowTenantListCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle():void
     {
         $modelClass = XotData::make()->getTenantClass();
 

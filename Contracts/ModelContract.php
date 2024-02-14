9094a7f5974bd309d9747ed8e342d038d1e33aa8
marco76tv <marco.sottana@gmail.com>
2024-02-14 12:33:52 +0100

    rebase 24/30

--- Contracts/ModelContract.php
+++ Contracts/ModelContract.php
@@ -32,6 +32,7 @@ interface ModelContract
     /**
      * Save the model to the database.
      *
+     * @param  array  $options
      * @return bool
      */
     public function save(array $options = []);

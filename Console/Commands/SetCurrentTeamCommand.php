d8916354b19617ffa79ba401ec460adfa8c30cd2
marco76tv <marco.sottana@gmail.com>
2024-02-22 15:57:56 +0100

    up

---
+++ Console/Commands/SetCurrentTeamCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\text;
+
+use Modules\User\Models\User;
+use Modules\Xot\Datas\XotData;
+use Symfony\Component\Console\Input\InputOption;
+use Webmozart\Assert\Assert;
+
+class SetCurrentTeamCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'user:set-current-team';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Assign current team to user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $email = text('email ?');
+        Assert::notNull($user = User::firstWhere(['email' => $email]));
+        $xot = XotData::make();
+        $teamClass = $xot->getTeamClass();
+
+        $opts = $teamClass::pluck('name', 'id');
+
+        $team_name = select(
+            label: 'What team?',
+            options: $opts,
+            required: true,
+            scroll: 10,
+            // validate: function (array $values) {
+            //  return ! \in_array(\count($values), [1, 2], true)
+            //    ? 'A maximum of two'
+            //  : null;
+            // }
+        );
+
+        $team = $teamClass::firstWhere('name', $team_name);
+
+        $user->current_team_id = $team->id;
+        $user->save();
+
+        // $user->teams()->sync($rows);
+        /*
+        foreach ($rows as $row) {
+            $role = Role::firstOrCreate(['name' => $row]);
+            $user->assignRole($role);
+        }
+        */
+        $this->info('OK');
+    }
+
+    /**
+     * Get the console command options.
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null],
+        ];
+    }
+}

ae42f34d3683f9d87ba495f06807f6f869071530
marco76tv <marco.sottana@gmail.com>
2024-02-23 16:59:19 +0100

    Refactor super admin badge display logic

--- Resources/views/badges/super-admin.blade.php
+++ Resources/views/badges/super-admin.blade.php
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 @if (isset($_profile) && $_profile->isSuperAdmin())
-=======
-@if(isset($_profile) && $_profile->isSuperAdmin())
->>>>>>> 3296a30 (up)
-   <x-filament::icon-button
-    icon="fas-chess-king"
-    class="h-5 w-5 text-gray-500 dark:text-gray-400"
-    tooltip="Super Admin"
-    />
+    <x-filament::icon-button icon="fas-chess-king" class="h-5 w-5 text-gray-500 dark:text-gray-400"
+        tooltip="Super Admin" />
 @endif

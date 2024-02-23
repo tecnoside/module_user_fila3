aa5e1eaf4be236637dbbc745430b973e85bc639f
marco76tv <marco.sottana@gmail.com>
2024-02-23 16:59:12 +0100

    Fix blade file syntax for checking super admin status

--- Resources/views/badges/super-admin.blade.php
+++ Resources/views/badges/super-admin.blade.php
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 @if (isset($_profile) && $_profile->isSuperAdmin())
+=======
+@if(isset($_profile) && $_profile->isSuperAdmin())
+>>>>>>> 3296a30 (up)
    <x-filament::icon-button
     icon="fas-chess-king"
     class="h-5 w-5 text-gray-500 dark:text-gray-400"

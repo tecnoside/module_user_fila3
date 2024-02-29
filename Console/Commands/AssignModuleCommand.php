e7d6494f009723c2107ca02266271d8edf9f4c24
marco76tv <marco.sottana@gmail.com>
2024-02-29 15:37:53 +0100

    up

--- Console/Commands/AssignModuleCommand.php
+++ Console/Commands/AssignModuleCommand.php
@@ -62,7 +62,7 @@ class AssignModuleCommand extends Command
             required: true,
             scroll: 10,
             // validate: function (array $values) {
-            //  return ! \in_array(\count($values), [1, 2], true)
+            //  return ! \in_array(\count($values), [1, 2], false)
             //    ? 'A maximum of two'
             //  : null;
             // }

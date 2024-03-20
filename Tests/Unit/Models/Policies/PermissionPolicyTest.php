10017e6beb61695017a8c6e3670b462759837487
Malebestia <vair81@gmail.com>
2024-03-20 16:08:41 +0100

    phpstan

--- Tests/Unit/Models/Policies/PermissionPolicyTest.php
+++ Tests/Unit/Models/Policies/PermissionPolicyTest.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\User\Tests\Unit\Models\Policies;
 
-use App\User;
-use Modules\User\Models\Policies\PermissionPolicy;
-use Modules\User\Models\User as UserAlias;
 use Tests\TestCase;
+use Modules\User\Models\User;
+use Modules\User\Models\User as UserAlias;
+use Modules\User\Models\Policies\PermissionPolicy;
 
 /**
  * Class PermissionPolicyTest.

11e7836af87971b9f514c4ce422c9063e94eb064
marco76tv <marco76tv@users.noreply.github.com>
2024-04-29 14:34:24 +0000

    Check & fix styling

--- Models/PasswordReset.php
+++ Models/PasswordReset.php
@@ -11,16 +11,16 @@ use Modules\User\Database\Factories\PasswordResetFactory;
 /**
  * Modules\User\Models\PasswordReset.
  *
- * @property int $id
- * @property string $email
- * @property string $token
+ * @property int         $id
+ * @property string      $email
+ * @property string      $token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property string|null $user_id
  * @property string|null $updated_by
  * @property string|null $created_by
  *
- * @method static PasswordResetFactory factory($count = null, $state = [])
+ * @method static PasswordResetFactory  factory($count = null, $state = [])
  * @method static Builder|PasswordReset newModelQuery()
  * @method static Builder|PasswordReset newQuery()
  * @method static Builder|PasswordReset query()
@@ -32,6 +32,7 @@ use Modules\User\Database\Factories\PasswordResetFactory;
  * @method static Builder|PasswordReset whereUpdatedAt($value)
  * @method static Builder|PasswordReset whereUpdatedBy($value)
  * @method static Builder|PasswordReset whereUserId($value)
+ *
  * @mixin \Eloquent
  */
 class PasswordReset extends BaseModel

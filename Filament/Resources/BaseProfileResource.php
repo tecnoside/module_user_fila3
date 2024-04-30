e2a1258d187c940315ea80d91a1f2d91674f5922
marco76tv <marco.sottana@gmail.com>
2024-04-30 12:37:20 +0200

    up

--- Filament/Resources/BaseProfileResource.php
+++ Filament/Resources/BaseProfileResource.php
@@ -24,7 +24,7 @@ abstract class BaseProfileResource extends XotBaseResource
 
     protected static ?string $model = BaseProfile::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     public static function form(Form $form): Form
     {
--- Filament/Resources/BaseProfileResource/Pages/ListProfiles.php
+++ Filament/Resources/BaseProfileResource/Pages/ListProfiles.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace Modules\User\Filament\Resources\BaseProfileResource\Pages;
 
 use Filament\Actions;
+use Filament\Tables\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Resources\Pages\ListRecords;
 use Modules\User\Filament\Resources\BaseProfileResource;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 
 class ListProfiles extends ListRecords
 {
@@ -17,5 +21,96 @@ class ListProfiles extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableActions():array{
+        return [
+            Tables\Actions\EditAction::make(),
+            Tables\Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getTableColumns():array{
+        return [
+            TextColumn::make('user.email'),
+            TextColumn::make('first_name'),
+            TextColumn::make('last_name'),
+            TextColumn::make('email'),
+            TextColumn::make('credits'),
+            SpatieMediaLibraryImageColumn::make('photo_profile')
+                ->collection('photo_profile'),
+        ];
+    }
+
+    protected function getTableBulkActions():array {
+        return [
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+                /*
+                Tables\Actions\BulkAction::make('refresh-profiles')
+                    ->requiresConfirmation()
+                    ->action(function (Collection $records) {
+                        $users = User::all();
+
+                        foreach ($users as $user) {
+                            Profile::firstOrCreate(
+                                ['user_id' => $user->id, 'email' => $user->email],
+                                ['credits' => 1000]
+                            );
+                        }
+                    }),
+                */
+            ]),
+        ];
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            //->query($this->getTableQuery())
+            ->actions($this->getTableActions())
+            //->actionsColumnLabel($this->getTableActionsColumnLabel())
+            //->checkIfRecordIsSelectableUsing($this->isTableRecordSelectable())
+            ->columns($this->getTableColumns())
+            //->columnToggleFormColumns($this->getTableColumnToggleFormColumns())
+            //->columnToggleFormMaxHeight($this->getTableColumnToggleFormMaxHeight())
+            //->columnToggleFormWidth($this->getTableColumnToggleFormWidth())
+            //->content($this->getTableContent())
+            //->contentFooter($this->getTableContentFooter())
+            //->contentGrid($this->getTableContentGrid())
+            //->defaultSort($this->getDefaultTableSortColumn(), $this->getDefaultTableSortDirection())
+            //->deferLoading($this->isTableLoadingDeferred())
+            //->description($this->getTableDescription())
+            //->deselectAllRecordsWhenFiltered($this->shouldDeselectAllRecordsWhenTableFiltered())
+            //->emptyState($this->getTableEmptyState())
+            //->emptyStateActions($this->getTableEmptyStateActions())
+            //->emptyStateDescription($this->getTableEmptyStateDescription())
+            //->emptyStateHeading($this->getTableEmptyStateHeading())
+            //->emptyStateIcon($this->getTableEmptyStateIcon())
+            //->filters($this->getTableFilters())
+            //->filtersFormMaxHeight($this->getTableFiltersFormMaxHeight())
+            //->filtersFormWidth($this->getTableFiltersFormWidth())
+            //->groupedBulkActions($this->getTableBulkActions())
+            ->bulkActions($this->getTableBulkActions())
+            //->header($this->getTableHeader())
+            //->headerActions($this->getTableHeaderActions())
+            //->modelLabel($this->getTableModelLabel())
+            //->paginated($this->isTablePaginationEnabled())
+            //->paginatedWhileReordering($this->isTablePaginationEnabledWhileReordering())
+            //->paginationPageOptions($this->getTableRecordsPerPageSelectOptions())
+            //->persistFiltersInSession($this->shouldPersistTableFiltersInSession())
+            //->persistSearchInSession($this->shouldPersistTableSearchInSession())
+            //->persistColumnSearchesInSession($this->shouldPersistTableColumnSearchInSession())
+            //->persistSortInSession($this->shouldPersistTableSortInSession())
+            //->pluralModelLabel($this->getTablePluralModelLabel())
+            //->poll($this->getTablePollingInterval())
+            //->recordAction($this->getTableRecordActionUsing())
+            //->recordClasses($this->getTableRecordClassesUsing())
+            //->recordTitle(fn (Model $record): ?string => $this->getTableRecordTitle($record))
+            //->recordUrl($this->getTableRecordUrlUsing())
+            //->reorderable($this->getTableReorderColumn())
+            //->selectCurrentPageOnly($this->shouldSelectCurrentPageOnly())
+            //->striped($this->isTableStriped())
+            ;
     }
 }

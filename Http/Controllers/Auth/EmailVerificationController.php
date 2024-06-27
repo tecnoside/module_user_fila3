8676ad371c07a1b14c49d1f37ddee2de5548c2da
marco76tv <marco76tv@users.noreply.github.com>
2024-06-27 15:00:33 +0000

    Dusting

--- Http/Controllers/Auth/EmailVerificationController.php
+++ Http/Controllers/Auth/EmailVerificationController.php
@@ -8,12 +8,11 @@
  * and that the email has not already been verified. If the verification is
  * successful, it marks the email as verified and dispatches a Verified event.
  *
- * @param string $id   the ID of the user to be verified
- * @param string $hash the hash of the user's email address
+ * @param  string  $id  the ID of the user to be verified
+ * @param  string  $hash  the hash of the user's email address
+ * @return \Illuminate\Http\RedirectResponse a redirect response to the home page
  *
  * @throws \Illuminate\Auth\Access\AuthorizationException if the verification fails
- *
- * @return \Illuminate\Http\RedirectResponse a redirect response to the home page
  */
 
 declare(strict_types=1);

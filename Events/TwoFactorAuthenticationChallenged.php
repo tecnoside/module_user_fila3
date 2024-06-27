791b7f0fb2890d42da55e85e9f1c4d8a457e37f2
marco76tv <marco76tv@users.noreply.github.com>
2024-06-27 15:00:33 +0000

    Dusting

--- Events/TwoFactorAuthenticationChallenged.php
+++ Events/TwoFactorAuthenticationChallenged.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\User\Events;
 
-class TwoFactorAuthenticationChallenged extends TwoFactorAuthenticationEvent
-{
-}
+class TwoFactorAuthenticationChallenged extends TwoFactorAuthenticationEvent {}

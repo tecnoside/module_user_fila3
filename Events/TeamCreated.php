e813d5d6946f3aef11efdade479dad55559a7d7a
marco76tv <marco76tv@users.noreply.github.com>
2024-06-27 15:00:33 +0000

    Dusting

--- Events/TeamCreated.php
+++ Events/TeamCreated.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\User\Events;
 
-class TeamCreated extends TeamEvent
-{
-}
+class TeamCreated extends TeamEvent {}
--- Events/TwoFactorAuthenticationEnabled.php
+++ Events/TwoFactorAuthenticationEnabled.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\User\Events;
 
-class TwoFactorAuthenticationEnabled extends TwoFactorAuthenticationEvent
-{
-}
+class TwoFactorAuthenticationEnabled extends TwoFactorAuthenticationEvent {}

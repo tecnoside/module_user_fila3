ddb48f2165933ed87b2df4bcbca56ac3dfa90aac
marco76tv <marco76tv@users.noreply.github.com>
2024-06-28 09:00:50 +0000

    Check & fix styling

--- Contracts/TenantContract.php
+++ Contracts/TenantContract.php
@@ -9,4 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @phpstan-require-extends Model
  */
-interface TenantContract extends ModelContract {}
+interface TenantContract extends ModelContract
+{
+}

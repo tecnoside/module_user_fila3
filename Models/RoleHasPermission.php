3682c281938079d89213de026fd3aa86123201aa
marco <marco.sottana@gmail.com>
2024-07-12 17:52:47 +0200

    up

--- Models/RoleHasPermission.php
+++ Models/RoleHasPermission.php
@@ -13,24 +13,20 @@ use Illuminate\Support\Carbon;
  * @property int $id
  * @property int $permission_id
  * @property int $role_id
- *
  * @method static Builder|RoleHasPermission newModelQuery()
  * @method static Builder|RoleHasPermission newQuery()
  * @method static Builder|RoleHasPermission query()
  * @method static Builder|RoleHasPermission whereId($value)
  * @method static Builder|RoleHasPermission wherePermissionId($value)
  * @method static Builder|RoleHasPermission whereRoleId($value)
- *
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  * @property string|null $updated_by
  * @property string|null $created_by
- *
  * @method static Builder|RoleHasPermission whereCreatedAt($value)
  * @method static Builder|RoleHasPermission whereCreatedBy($value)
  * @method static Builder|RoleHasPermission whereUpdatedAt($value)
  * @method static Builder|RoleHasPermission whereUpdatedBy($value)
- *
  * @mixin \Eloquent
  */
 class RoleHasPermission extends BasePivot

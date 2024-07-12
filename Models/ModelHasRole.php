5e82970b3043fb64d5aa2689b8a452db899dcf36
marco <marco.sottana@gmail.com>
2024-07-12 23:09:31 +0200

    up

--- Models/ModelHasRole.php
+++ Models/ModelHasRole.php
@@ -20,7 +20,6 @@ use Modules\User\Database\Factories\ModelHasRoleFactory;
  * @property Carbon|null $updated_at
  * @property string|null $updated_by
  * @property string|null $created_by
- *
  * @method static ModelHasRoleFactory  factory($count = null, $state = [])
  * @method static Builder|ModelHasRole newModelQuery()
  * @method static Builder|ModelHasRole newQuery()
@@ -34,11 +33,8 @@ use Modules\User\Database\Factories\ModelHasRoleFactory;
  * @method static Builder|ModelHasRole whereTeamId($value)
  * @method static Builder|ModelHasRole whereUpdatedAt($value)
  * @method static Builder|ModelHasRole whereUpdatedBy($value)
- *
  * @property string $uuid (DC2Type:guid)
- *
  * @method static Builder|ModelHasRole whereUuid($value)
- *
  * @mixin \Eloquent
  */
 class ModelHasRole extends BaseMorphPivot

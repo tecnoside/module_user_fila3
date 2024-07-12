c6db8fb1dbee105f45e0c0b4625a9bebc9c6493b
marco76tv <marco76tv@users.noreply.github.com>
2024-07-12 15:02:55 +0000

    Check & fix styling

--- Models/Extra.php
+++ Models/Extra.php
@@ -7,13 +7,13 @@ namespace Modules\User\Models;
 use Modules\Xot\Models\Extra as XotBaseExtra;
 
 /**
- * 
- *
  * @property \Spatie\SchemalessAttributes\SchemalessAttributes $extra_attributes
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|Extra newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Extra newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Extra query()
- * @method static Builder|Extra withExtraAttributes()
+ * @method static Builder|Extra                               withExtraAttributes()
+ *
  * @mixin \Eloquent
  */
 class Extra extends XotBaseExtra

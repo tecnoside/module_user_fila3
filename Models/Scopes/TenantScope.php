3ee49c91a936de8edbfbde72d630afd89e2513b6
marco <marco.sottana@gmail.com>
2024-07-30 16:36:58 +0200

    up

---
+++ Models/Scopes/TenantScope.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models\Scopes;
+
+use Filament\Facades\Filament;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class TenantScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        $builder->where('tenant_id', '=', Filament::getTenant()->id);
+    }
+}

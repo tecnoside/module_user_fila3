6548158d56dd0f23c977d47b9a176f1cb6f2e421
marco <marco.sottana@gmail.com>
2024-08-05 12:44:43 +0200

    up

--- Filament/Resources/DeviceResource/Pages/ListDevices.php
+++ Filament/Resources/DeviceResource/Pages/ListDevices.php
@@ -6,11 +6,22 @@ namespace Modules\User\Filament\Resources\DeviceResource\Pages;
 
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 use Modules\User\Filament\Resources\DeviceResource;
 
 class ListDevices extends ListRecords
 {
     protected static string $resource = DeviceResource::class;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
--- Filament/Resources/TeamResource/Pages/ListTeams.php
+++ Filament/Resources/TeamResource/Pages/ListTeams.php
@@ -6,12 +6,23 @@ namespace Modules\User\Filament\Resources\TeamResource\Pages;
 
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Modules\UI\Enums\TableLayoutEnum;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 use Modules\User\Filament\Resources\TeamResource;
 
 class ListTeams extends ListRecords
 {
     // //
     protected static string $resource = TeamResource::class;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::GRID;
+
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            TableLayoutToggleTableAction::make(),
+        ];
+    }
 
     protected function getHeaderActions(): array
     {

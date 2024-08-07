0905b715a9a81a8191b1eb3a3550b394c7c99779
marco76tv <marco.sottana@gmail.com>
2024-08-07 14:33:25 +0200

    .

---
+++ Filament/Clusters/Appearance.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\User\Filament\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Appearance extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+}

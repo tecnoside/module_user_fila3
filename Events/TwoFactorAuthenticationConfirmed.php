b0e1cc6caaf5cb624d5cd015515196559012658c
marco76tv <marco76tv@users.noreply.github.com>
2024-08-21 08:29:25 +0000

    Check & fix styling

--- Events/TwoFactorAuthenticationConfirmed.php
+++ Events/TwoFactorAuthenticationConfirmed.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\User\Events;
 
-class TwoFactorAuthenticationConfirmed extends TwoFactorAuthenticationEvent {}
+class TwoFactorAuthenticationConfirmed extends TwoFactorAuthenticationEvent
+{
+}

76a9176a239e93550ad71ae892875b3600bf3244
marco <marco.sottana@gmail.com>
2024-08-27 18:18:34 +0200

    .

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\User\Providers;
 
+use Illuminate\Auth\Events\Failed;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
+use Illuminate\Auth\Events\OtherDeviceLogout;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Modules\User\Listeners\FailedLoginListener;
 use Modules\User\Listeners\LoginListener;
 use Modules\User\Listeners\LogoutListener;
+use Modules\User\Listeners\OtherDeviceLogoutListener;
 use SocialiteProviders\Auth0\Auth0ExtendSocialite;
 use SocialiteProviders\Manager\SocialiteWasCalled;
 
@@ -28,6 +32,12 @@ class EventServiceProvider extends ServiceProvider
         ],
         Logout::class => [
             LogoutListener::class,
+        ],
+        Failed::class => [
+            FailedLoginListener::class,
+        ],
+        OtherDeviceLogout::class => [
+            OtherDeviceLogoutListener::class,
         ],
     ];
 }

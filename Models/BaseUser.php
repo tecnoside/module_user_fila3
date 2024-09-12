1a0503c36013856d0b4a1120f49ccf2d5b39cbbe
Malebestia <vair81@gmail.com>
2024-09-12 17:17:34 +0200

    rebase 2/5

--- Models/BaseUser.php
+++ Models/BaseUser.php
@@ -252,6 +252,11 @@ abstract class BaseUser extends Authenticatable implements HasName, HasTenants, 
         return true; // str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
     }
 
+    public function canAccessSocialite(): bool
+    {
+        return true;
+    }
+
     /*
     public function mobileDevices(): BelongsToMany
     {
@@ -319,10 +324,5 @@ abstract class BaseUser extends Authenticatable implements HasName, HasTenants, 
         $this->update(['name' => $value]);
 
         return $value;
-    }
-
-    public function canAccessSocialite(): bool
-    {
-        return true;
     }
 }

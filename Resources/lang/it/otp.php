8f949e6d633e2cd50dade7b42b45ffa60c7ab914
marco <marco.sottana@gmail.com>
2024-09-20 17:46:38 +0200

    📝 (otp.php): Add new language keys and messages for password reset functionality 🌐 (otp.php): Update language keys and messages for password reset functionality to support internationalization and localization

--- Resources/lang/it/otp.php
+++ Resources/lang/it/otp.php
@@ -4,6 +4,37 @@ declare(strict_types=1);
 
 return [
     'otp_code' => 'OTP Code',
+    'title' => 'Password Expired, Reset Password',
+    'heading' => 'Create A New Password',
+    'sub_heading' => 'Your Password Has Expired, Please Create A New Password',
+    'form' => [
+        'current_password' => [
+            'label' => 'Current Password',
+            'validation_attribute' => 'current_password',
+        ],
+        'password' => [
+            'label' => 'Password',
+            'validation_attribute' => 'password',
+        ],
+        'password_confirmation' => [
+            'label' => 'Confirm Password',
+        ],
+    ],
+    'fields' => [
+        'current_password' => [
+            'label' => 'Current Password',
+            'validation_attribute' => 'current_password',
+        ],
+        'password' => [
+            'label' => 'Password',
+            'validation_attribute' => 'password',
+        ],
+        'password_confirmation' => [
+            'label' => 'Confirm Password',
+        ],
+    ],
+    'reset_password' => 'Reset Password',
+    'password_reset' => 'Password Reset',
 
     'mail' => [
         'subject' => 'OTP Code',
@@ -24,6 +55,24 @@ return [
     'notifications' => [
         'title' => 'OTP Code Sent',
         'body' => 'The verification code has been sent to your e-mail address. It will be valid in :seconds seconds.',
+        'wrong_password' => [
+            'title' => 'Wrong Password',
+            'body' => 'The current password you entered is incorrect.',
+        ],
+        'column_not_found' => [
+            'title' => 'Column Not Found',
+            'body' => 'Either the column ":column_name" or the password column ":password_column_name" was not found in the :table_name table.',
+        ],
+        'password_reset' => [
+            'success' => 'Password Reset Successful',
+        ],
+        'same_password' => [
+            'title' => 'Same Password',
+            'body' => 'The new password must be different from the current password.',
+        ],
+    ],
+    'exceptions' => [
+        'column_not_found' => 'Either the column ":column_name" or the password column ":password_column_name" was not found in the ":table_name" table. Please publish migrations and run them, if the error still persists, publish the config file and update the table_name, column_name, and password_column_name values.',
     ],
 
     'validation' => [

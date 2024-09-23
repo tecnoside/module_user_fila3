d998f770832da67c9fb9605d716cd3d0c33419c2
marco <marco.sottana@gmail.com>
2024-09-23 18:33:44 +0200

    up

--- Datas/PasswordData.php
+++ Datas/PasswordData.php
@@ -20,6 +20,14 @@ class PasswordData extends Data
     public int $otp_length;  // Lunghezza del codice OTP
     public int $expires_in; // The number of days before the password expires.
 
+    public int $min = 6; // The minimum size of the password.
+    public bool $mixedCase = false; // If the password requires at least one uppercase and one lowercase letter.
+    public bool $letters = false; // If the password requires at least one letter.
+    public bool $numbers = false; // If the password requires at least one number.
+    public bool $symbols = false; // If the password requires at least one symbol.
+    public bool $uncompromised = false; // If the password should not have been compromised in data leaks.
+    public int $compromisedThreshold = 1; // The number of times a password can appear in data leaks before being considered compromised.
+
     private static ?self $instance = null;
 
     public static function make(): self

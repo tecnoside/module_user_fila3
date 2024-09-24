158f144e91820ea4e3617a0dd1bdbc4a64b2e0e4
marco76tv <marco.sottana@gmail.com>
2024-09-24 10:50:13 +0200

    .

--- Models/Device.php
+++ Models/Device.php
@@ -71,6 +71,8 @@ class Device extends BaseModel
     protected function casts(): array
     {
         return [
+            'id' => 'string',
+            'uuid' => 'string',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',

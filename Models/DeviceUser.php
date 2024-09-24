8879e787f6ec4dc196e9cb52c16a7f31761e4a9d
marco76tv <marco76tv@users.noreply.github.com>
2024-09-24 10:47:04 +0000

    Check & fix styling

--- Models/DeviceUser.php
+++ Models/DeviceUser.php
@@ -16,9 +16,11 @@ use Modules\Xot\Datas\XotData;
  * Modules\User\Models\DeviceUser.
  *
  * @property Device|null $device
+ *
  * @method static Builder|DeviceUser newModelQuery()
  * @method static Builder|DeviceUser newQuery()
  * @method static Builder|DeviceUser query()
+ *
  * @property int         $id
  * @property string      $device_id
  * @property string      $user_id
@@ -30,6 +32,7 @@ use Modules\Xot\Datas\XotData;
  * @property Carbon|null $updated_at
  * @property string|null $updated_by
  * @property string|null $created_by
+ *
  * @method static Builder|DeviceUser whereCreatedAt($value)
  * @method static Builder|DeviceUser whereCreatedBy($value)
  * @method static Builder|DeviceUser whereDeviceId($value)
@@ -41,10 +44,12 @@ use Modules\Xot\Datas\XotData;
  * @method static Builder|DeviceUser whereUpdatedAt($value)
  * @method static Builder|DeviceUser whereUpdatedBy($value)
  * @method static Builder|DeviceUser whereUserId($value)
+ *
  * @property ProfileContract|null $profile
  * @property UserContract|null    $user
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
+ *
  * @mixin \Eloquent
  */
 class DeviceUser extends BasePivot

fe7edf7354286bb32c8526c2451b01708a873383
marco76tv <marco76tv@users.noreply.github.com>
2024-09-25 11:13:37 +0000

    Check & fix styling

--- Contracts/TenantContract.php
+++ Contracts/TenantContract.php
@@ -6,7 +6,6 @@ namespace Modules\User\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 /**
  * @method static \Modules\User\Database\Factories\TenantFactory factory($count = null, $state = [])
@@ -15,9 +14,10 @@ use Illuminate\Database\Eloquent\Collection as EloquentCollection;
  * @method static \Illuminate\Database\Eloquent\Builder|Tenant   query()
  *
  * @property \Illuminate\Database\Eloquent\Collection<int, Model&UserContract> $members
- * @property int|null                                    $members_count
- * @property \Modules\Xot\Contracts\ProfileContract|null $creator
- * @property \Modules\Xot\Contracts\ProfileContract|null $updater
+ * @property int|null                                                          $members_count
+ * @property \Modules\Xot\Contracts\ProfileContract|null                       $creator
+ * @property \Modules\Xot\Contracts\ProfileContract|null                       $updater
+ *
  * @phpstan-require-extends Model
  */
 interface TenantContract extends ModelContract

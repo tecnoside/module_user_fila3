2f85516256f6c7032d95995ecadf95478574c0e4
marco <marco.sottana@gmail.com>
2024-09-27 16:55:25 +0200

    up

--- Notifications/Auth/ResetPassword.php
+++ Notifications/Auth/ResetPassword.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Modules\User\Notifications\Auth;
 
 use Illuminate\Auth\Notifications\ResetPassword as BaseNotification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Lang;
 
 class ResetPassword extends BaseNotification
 {
@@ -13,5 +16,22 @@ class ResetPassword extends BaseNotification
     protected function resetUrl($notifiable): string
     {
         return $this->url;
+    }
+
+    /**
+     * Get the reset password notification mail message for the given URL.
+     *
+     * @param string $url
+     *
+     * @return MailMessage
+     */
+    protected function buildMailMessage($url)
+    {
+        return (new MailMessage())
+            ->subject(Lang::get('user::email.password_reset_subject'))
+            ->line(Lang::get('user::email.password_cause_of_email'))
+            ->action(Lang::get('user::email.reset_password'), $url)
+            ->line(Lang::get('user::email.password_reset_expiration', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('user::email.password_if_not_requested'));
     }
 }

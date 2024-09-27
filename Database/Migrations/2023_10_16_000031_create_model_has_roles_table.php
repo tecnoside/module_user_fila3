c9cca35a53cca0f7b2246317d81c1b4800fa1422
marco <marco.sottana@gmail.com>
2024-09-27 18:04:16 +0200

    .

--- Database/Migrations/2023_10_16_000031_create_model_has_roles_table.php
+++ Database/Migrations/2023_10_16_000031_create_model_has_roles_table.php
@@ -10,7 +10,7 @@ use Modules\Xot\Datas\XotData;
 /*
  * Class CreateModelHasRolesTable.
  */
-return new class extends XotBaseMigration {
+return new class() extends XotBaseMigration {
     /**
      * Run the migrations.
      */
@@ -32,6 +32,9 @@ return new class extends XotBaseMigration {
                 $team_class = XotData::make()->getTeamClass();
                 if (! $this->hasColumn('team_id')) {
                     $table->foreignIdFor($team_class, 'team_id')->nullable();
+                }
+                if ('uuid' == $this->getColumnType('model_id')) {
+                    $table->string('model_id', 36)->index()->change();
                 }
                 // $this->updateUser($table);
                 $this->updateTimestamps($table);

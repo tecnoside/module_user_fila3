7c5a998e54cfed8023a3eb37834dcf9990a595db
marco76tv <marco76tv@users.noreply.github.com>
2024-10-11 10:16:46 +0000

    Check & fix styling

--- Filament/Widgets/PasswordExpiredWidget.php
+++ Filament/Widgets/PasswordExpiredWidget.php
@@ -4,26 +4,26 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Widgets;
 
-use Filament\Forms\Form;
-use Illuminate\Support\Arr;
 use Filament\Actions\Action;
-use Filament\Widgets\Widget;
-use Webmozart\Assert\Assert;
 use Filament\Actions\ActionGroup;
-use Illuminate\Support\Facades\Hash;
-use Modules\User\Datas\PasswordData;
 use Filament\Forms\ComponentContainer;
-use Filament\Forms\Contracts\HasForms;
-use Illuminate\Support\Facades\Schema;
-use Modules\User\Events\NewPasswordSet;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
-use Filament\Notifications\Notification;
-use Modules\Xot\Filament\Traits\TransTrait;
 use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Pages\Concerns\InteractsWithFormActions;
-use Modules\User\Http\Response\PasswordResetResponse;
+use Filament\Widgets\Widget;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Validation\Rules\Password as PasswordRule;
+use Modules\User\Datas\PasswordData;
+use Modules\User\Events\NewPasswordSet;
+use Modules\User\Http\Response\PasswordResetResponse;
+use Modules\Xot\Filament\Traits\TransTrait;
+use Webmozart\Assert\Assert;
 
 /**
  * @property ComponentContainer $form
@@ -126,11 +126,11 @@ class PasswordExpiredWidget extends Widget implements HasForms
             return null;
         }
 
-        $pwd_data=PasswordData::make();
+        $pwd_data = PasswordData::make();
         // get OTP expiration minutes from PasswordData
         $otpExpirationMinutes = $pwd_data->otp_expiration_minutes;
 
-         // Check if OTP is expired using updated_at
+        // Check if OTP is expired using updated_at
         if ($user->updated_at && now()->greaterThan($user->updated_at->addMinutes($otpExpirationMinutes))) {
             Notification::make()
                 ->title(__('user::otp.notifications.otp_expired.title'))
@@ -140,7 +140,6 @@ class PasswordExpiredWidget extends Widget implements HasForms
 
             return null;
         }
-
 
         // get password expiry date and time
         $passwordExpiryDateTime = now()->addDays($pwd_data->expires_in);

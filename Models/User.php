5fd7aab222e1486082b3f0f69a78d4d6fe8406de
marco76tv <marco76tv@users.noreply.github.com>
2024-10-15 09:02:05 +0000

    Check & fix styling

--- Models/User.php
+++ Models/User.php
@@ -7,11 +7,12 @@ namespace Modules\User\Models;
 use Modules\Xot\Contracts\ProfileContract;
 
 /**
-<<<<<<< HEAD
+ * <<<<<<< HEAD
  * Class Modules\User\Models\User.
  *
-=======
->>>>>>> 0dc79bab2a130fa78fc24d39d3e36a89a76e94c8
+ * =======
+ * >>>>>>> 0dc79bab2a130fa78fc24d39d3e36a89a76e94c8
+ *
  * @property string                                                                                           $id
  * @property string|null                                                                                      $name
  * @property string|null                                                                                      $first_name

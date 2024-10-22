123da71d162d0f3d903b8afdb74cd6ea988c1ede
marco <marco.sottana@gmail.com>
2024-10-22 16:53:26 +0200

    This code defines a new class called `UsersRelationManager` that extends the existing `RelationManager` class. The new class adds some additional functionality to the existing manager, such as defining a custom form schema and table columns for the users relation.
     Here's a breakdown of the changes made in this code:
    
     * The `use TransTrait;` line imports the `TransTrait` trait from the `Laravel\Nova\Http\Controllers\ResourceShowController` class, which provides some useful methods for working with translations.
     * The `$layoutView` property is set to `TableLayoutEnum::LIST`, which tells Nova to use the list layout for this relation manager.
     * The `protected static string $relationship = 'users';` line sets the name of the relationship that this manager will manage. In this case, it's the "users" relationship on the tenant model.
     * The `protected static ?string $recordTitleAttribute = 'name';` line sets the attribute that Nova should use as the title for each record in the relation. In this case, it's the "name" attribute of the user model.
     * The `form()` method is overridden to define a custom form schema for creating and editing users. This method returns a new instance of the `Form` class with the schema defined by the `getFormSchema()` method.
     * The `getFormSchema()` method defines the form schema as an array of `Forms\Components\TextInput` components, each representing a field in the user model. The `label()`, `required()`, and `maxLength()` methods are used to customize the label, required status, and maximum length for each input field.
     * The `table()` method is overridden to define a custom table layout for displaying users. This method returns a new instance of the `Table` class with the columns defined by the `getListTableColumns()` method.
     * The `getListTableColumns()` method defines an array of `Tables\Columns\TextColumn` components, each representing a column in the user table. The `label()`, `searchable()`, and `sortable()` methods are used to customize the label, searchability, and sortability for each column.
     * The `getHeaderActions()` method is overridden to define an array of header actions that will be displayed above the table. In this case, it includes a "Create User" action that will open a form for creating a new user.
     * The `getTableHeaderActions()` method defines an array of table header actions that will be displayed in the top-right corner of the table. In this case, it includes a "Toggle Table Layout" action that will switch between list and card layouts, as well as a "Create User" action that will open a form for creating a new user.
     * The `getTableActions()` method defines an array of row-level actions that will be displayed in the table. In this case, it includes an "Edit User" action that will open a form for editing a selected user, as well as a "Delete User" action that will delete a selected user.
     * The `getTableBulkActions()` method defines an array of bulk actions that will be displayed in the table. In this case, it includes a "Delete Selected" action that will delete all selected users at once.

--- Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
+++ Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
@@ -7,63 +7,123 @@ namespace Modules\User\Filament\Resources\RoleResource\RelationManagers;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables\Actions\AttachAction;
+use Filament\Tables\Actions\DetachAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\ActionsPosition;
+use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Table;
+use Modules\UI\Enums\TableLayoutEnum;
 use Modules\User\Filament\Resources\UserResource;
+use Modules\Xot\Filament\Traits\TransTrait;
 
 class UsersRelationManager extends RelationManager
 {
+    use TransTrait;
+
+    public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
+
     protected static string $relationship = 'users';
 
-    // protected static ?string $inverseRelationship = 'teams';
+    protected static ?string $inverseRelationship = 'roles';
 
     protected static ?string $recordTitleAttribute = 'name';
 
+    /**
+     * Define the form for this relation.
+     */
     public function form(Form $form): Form
     {
+        return $this->getUserResourceForm($form);
+    }
+
+    /**
+     * Modular function to configure the form.
+     */
+    protected function getUserResourceForm(Form $form): Form
+    {
+        // Centralize form structure using UserResource for consistency
         return UserResource::form($form);
-        /*
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-            ]);
-        */
     }
 
     public function table(Table $table): Table
     {
-        $table = UserResource::table($table);
-        $columns = $table->getColumns();
-        $columns = collect($columns)->except(['teams.name', 'role.name', 'roles.name'])->all();
-        $columns['role'] = TextColumn::make('role');
-        $table->columns($columns);
-        $headerActions = $table->getHeaderActions();
-        $headerActions['attach'] = AttachAction::make();
-
-        return $table->headerActions($headerActions);
-        /*
         return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('name'),
-            ])
-            ->filters([
-            ])
-            ->headerActions([
-                Tables\Actions\CreateAction::make(),
-                Tables\Actions\AttachAction::make(),
-            ])
-            ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DetachAction::make(),
-                Tables\Actions\DeleteAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\DetachBulkAction::make(),
-                Tables\Actions\DeleteBulkAction::make(),
-            ]);
-        */
+            // ->columns($this->getTableColumns())
+            ->columns($this->layoutView->getTableColumns())
+            ->contentGrid($this->layoutView->getTableContentGrid())
+            ->headerActions($this->getTableHeaderActions())
+
+            ->filters($this->getTableFilters())
+            ->filtersLayout(FiltersLayout::AboveContent)
+            ->persistFiltersInSession()
+            ->actions($this->getTableActions())
+            ->bulkActions($this->getTableBulkActions())
+            ->actionsPosition(ActionsPosition::BeforeColumns)
+            ->defaultSort(
+                column: 'users.created_at',
+                direction: 'DESC',
+            );
+    }
+
+    public function getGridTableColumns(): array
+    {
+        return [
+            Stack::make($this->getListTableColumns()),
+        ];
+    }
+
+    public function getListTableColumns(): array
+    {
+        return [
+            TextColumn::make('name')
+                ->label(__('user.name'))
+                ->searchable()
+                ->sortable(),
+            TextColumn::make('email')
+                ->label(__('user.email'))
+                ->searchable()
+                ->sortable(),
+            TextColumn::make('role')
+                ->label(__('user.role')),
+        ];
+    }
+
+    /**
+     * Define the header actions in a separate function.
+     */
+    protected function getTableHeaderActions(): array
+    {
+        return [
+            AttachAction::make()
+                ->label('')
+                ->tooltip(__('role.attach_user'))
+                ->icon('heroicon-o-link')
+            // ->icon('heroicon-o-paper-clip')
+            ,
+        ];
+    }
+
+    /**
+     * Define the row-level actions in a separate function.
+     */
+    protected function getTableActions(): array
+    {
+        return [
+            ViewAction::make()
+                ->label('')
+                ->tooltip(__('role.view_user'))
+                ->icon('heroicon-o-eye'),
+            EditAction::make()
+                ->label('')
+                ->tooltip(__('role.edit_user'))
+                ->icon('heroicon-o-pencil'),
+            DetachAction::make()
+                ->label('')
+                ->tooltip(__('role.detach_user'))
+                ->icon('heroicon-o-link-slash'),
+        ];
     }
 }

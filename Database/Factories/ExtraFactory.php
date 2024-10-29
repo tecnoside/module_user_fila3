e4c6a4126f54550c2c4ce2c733289fad0c3728c1
marco76tv <marco.sottana@gmail.com>
2024-10-29 11:31:42 +0100

    up

--- Database/Factories/ExtraFactory.php
+++ Database/Factories/ExtraFactory.php
@@ -5,27 +5,19 @@ declare(strict_types=1);
 namespace Modules\User\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Database\Eloquent\Model;
-use Modules\User\Models\Extra;
 
 class ExtraFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
-     *
-     * @var class-string<Model>
      */
-    protected $model = Extra::class;
+    protected $model = \Modules\User\Models\Extra::class;
 
     /**
      * Define the model's default state.
      */
     public function definition(): array
     {
-        return [
-            // 'user_id' => $this->faker->randomNumber(5),
-            'name' => $this->faker->name,
-            'personal_team' => $this->faker->boolean,
-        ];
+        return [];
     }
 }
